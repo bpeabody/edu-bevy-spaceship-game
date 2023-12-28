a453cc8c399897267ab4d2a4ed81c6608d60066b
Brock Peabody <brock.peabody@gmail.com>
2023-12-28 11:13:03 -0500

    wip

---
+++ game-main/src/chat/interact.rs
@@ -0,0 +1,25 @@
+//use serde::Deserialize;
+//use reqwest::Error;
+//
+//#[derive(Deserialize, Debug)]
+//struct User {
+//    login: String,
+//    id: u32,
+//}
+
+//#[tokio::main]
+//pub async fn try_it() -> Result<(), Error> {
+//    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
+//                              owner = "rust-lang-nursery",
+//                              repo = "rust-cookbook");
+//    println!("{}", request_url);
+//    let response = reqwest::get(&request_url).await?;
+//
+//    let users: Vec<User> = response.json().await?;
+//    println!("{:?}", users);
+//    Ok(())
+//}
+
+pub fn try_it() {
+    println!("Hello world");
+}
--- game-main/src/main.rs
+++ game-main/src/main.rs
@@ -1,32 +1,36 @@
-mod bundles;
-mod components;
-mod plugins;
+// mod bundles;
+// mod components;
+// mod plugins;
+mod chat;
+// 
+// #[cfg(feature = "debug")]
+// use crate::plugins::DebugPlugin;
+// use crate::plugins::{
+//     AssetLoaderPlugin, AsteroidPlugin, CollisionDetectionPlugin, DespawnPlugin, MovementPlugin,
+//     SpaceshipPlugin,
+// };
+use crate::chat::interact::try_it;
 
-#[cfg(feature = "debug")]
-use crate::plugins::DebugPlugin;
-use crate::plugins::{
-    AssetLoaderPlugin, AsteroidPlugin, CollisionDetectionPlugin, DespawnPlugin, MovementPlugin,
-    SpaceshipPlugin,
-};
-
-use game_camera::CameraPlugin;
-
-use bevy::prelude::*;
+//use game_camera::CameraPlugin;
+//
+//use bevy::prelude::*;
 
 fn main() {
-    let mut app = App::new();
-    let app = app.add_plugins(DefaultPlugins).add_plugins((
-        AssetLoaderPlugin,
-        AsteroidPlugin,
-        CameraPlugin,
-        CollisionDetectionPlugin,
-        DespawnPlugin,
-        MovementPlugin,
-        SpaceshipPlugin,
-    ));
+    try_it()
 
-    #[cfg(feature = "debug")]
-    app.add_plugins(DebugPlugin);
-
-    app.run();
+//    let mut app = App::new();
+//    let app = app.add_plugins(DefaultPlugins).add_plugins((
+//        AssetLoaderPlugin,
+//        AsteroidPlugin,
+//        CameraPlugin,
+//        CollisionDetectionPlugin,
+//        DespawnPlugin,
+//        MovementPlugin,
+//        SpaceshipPlugin,
+//    ));
+//
+//    #[cfg(feature = "debug")]
+//    app.add_plugins(DebugPlugin);
+//
+//    app.run();
 }
